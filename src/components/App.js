6eb0f0c3608f3253134effc91f4617319eb86d0a
Mark Manalo <mmanalo@dealersocket.com>
2018-07-19 18:23:14 -0700

    removed prop validation, added App.js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+// This component handles the App template used on every page
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <p>Header here...</p>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default App;
