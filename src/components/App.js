6d83a299b542b5d341ad2bd12923b9fd1a75b12d
Mark Manalo <mmanalo@dealersocket.com>
2018-07-26 16:12:30 -0700

    added preloader and hid it when a call was in progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 // This component handles the App template used on every page
 import React from 'react';
 import Header from './common/Header';
+import { connect } from 'react-redux';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <Header/>
+        <Header loading={this.props.loading} />
         {this.props.children}
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state, ownPRops) {
+  return {
+    loading: state.ajaxCallsInProgress > 0
+  };
+}
+
+export default connect(mapStateToProps)(App);
